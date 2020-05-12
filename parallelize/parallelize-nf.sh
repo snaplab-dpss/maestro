8024216e9e53912394089b0ca025995a18b7df94
Francisco Pereira <fchamicapereira@gmail.com>
2020-05-12 15:15:58 +0100

    added parallelize script

---
+++ parallelize/parallelize-nf.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Bash "strict mode"
+set -euo pipefail
+
+NF_DIR=`pwd`
+BUILD="$NF_DIR/build/parallelization"
+KLEE_DIR="$VIGOR_DIR/klee"
+
+# ==============
+# Pre requisites
+# ==============
+
+cd "$NF_DIR"
+
+if [ $(ls -dq "$NF_DIR/klee-last" 2> /dev/null | wc -l) -eq "0" ]; then
+    echo "ERROR: no call paths to parse. Run \"make symbex\" first."
+    exit 1
+fi
+
+# ===========
+# Build setup
+# ===========
+
+mkdir -p "$BUILD"
+
+echo "Building parse-libvig-access"
+
+Z3_DIR="$VIGOR_DIR/z3"
+R3S_DIR="$VIGOR_DIR/libr3s"
+
+Z3_LIB_FLAGS="-lz3 -Wl,-rpath,$Z3_DIR/build/lib -L$Z3_DIR/build/lib -I$Z3_DIR/build/include"
+R3S_LIB_FLAGS="-lr3s -Wl,-rpath,$R3S_DIR/build/lib -L$R3S_DIR/build/lib -I$R3S_DIR/include"
+
+g++ "$NF_DIR/../parallelize/parse-libvig-access.c" \
+    -o "$BUILD/parse-libvig-access" \
+    $Z3_LIB_FLAGS \
+    $R3S_LIB_FLAGS
+
+echo "Building load-call-paths"
+
+cd "$KLEE_DIR"
+./build.sh > "$BUILD/klee-build-log.txt" 2>&1
+ln -sf "$KLEE_DIR/build/bin/load-call-paths" "$BUILD/load-call-paths"
+cd "$NF_DIR"
+
+# ================
+# Parse call paths
+# ================
+
+echo "Parsing call paths"
+CALL_PATHS=$NF_DIR/klee-last/test*.call_path
+"$BUILD/load-call-paths" $CALL_PATHS \
+    2> "$BUILD/livbig-access-log.txt" \
+    > "$BUILD/livbig-access-out.txt"
+
+echo "Parsing libvig report"
+"$BUILD/parse-libvig-access" "$BUILD/livbig-access-out.txt"
---
+++ parallelize/parse-libvig-access.c
@@ -0,0 +1,185 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <r3s.h>
+
+typedef struct {
+    unsigned layer;
+    unsigned proto;
+    R3S_pf_t *dep;
+    unsigned dep_sz;
+} libvig_access_t;
+
+bool match_token(char *str, const char* token) {
+    return strncmp(str, token, strlen(token)) == 0;
+}
+
+bool is_dep_in_array(R3S_pf_t pf, R3S_pf_t *pfs, unsigned sz) {
+    for (int i = 0; i < sz; i++)
+        if (pfs[i] == pf) return true;
+    return false;
+}
+
+void unique_save_access(
+    libvig_access_t access,
+    libvig_access_t **accesses,
+    unsigned *sz
+) {
+    libvig_access_t *curr;
+
+    if (*sz > 0) {
+        for (unsigned i = 0; i < *sz; i++) {
+            curr = &((*accesses)[i]);
+            if (curr->layer  != access.layer)  continue;
+            if (curr->proto  != access.proto)  continue;
+            if (curr->dep_sz != access.dep_sz) continue;
+
+            bool eq = true;
+            for (int j = 0; j < curr->dep_sz; j++) {
+                if (!is_dep_in_array(curr->dep[j], access.dep, access.dep_sz)) {
+                    eq = false;
+                    break;
+                }
+            }
+            if (!eq) continue;
+
+            return;
+        }
+    }
+
+    *sz += 1;
+    *accesses = (libvig_access_t*) realloc(
+        *accesses,
+        sizeof(libvig_access_t) * (*sz));
+    curr = &((*accesses)[*sz - 1]);
+
+    curr->layer  = access.layer;
+    curr->proto  = access.proto;
+    curr->dep_sz = access.dep_sz;
+    curr->dep    = access.dep;
+}
+
+void unique_save_dep(libvig_access_t *access, R3S_pf_t pf) {
+    assert(access != NULL);
+
+    if (is_dep_in_array(pf, access->dep, access->dep_sz))
+        return;
+    
+    access->dep_sz++;
+    access->dep = (R3S_pf_t*) realloc(
+        access->dep,
+        sizeof(R3S_pf_t) * access->dep_sz
+    );
+    access->dep[access->dep_sz - 1] = pf;
+}
+
+void parse_dep(libvig_access_t *access, unsigned dep) {
+    // IPv4
+    if (access->layer == 3 && access->proto == 0x0800) {
+        if (dep >= 12 && dep <= 15) {
+            unique_save_dep(access, R3S_PF_IPV4_SRC);
+        }
+
+        else if (dep >= 16 && dep <= 19) {
+            unique_save_dep(access, R3S_PF_IPV4_DST);
+        }
+
+        else {
+            printf("[WARNING] Unknown packet header field at byte %u\n", dep);
+        }
+    }
+
+    // IPv6
+    else if (access->layer == 3 && access->proto == 0x86DD) {
+
+    }
+
+    // VLAN
+    else if (access->layer == 3 && access->proto == 8100) {
+
+    }
+}
+
+void parse_libvig_access_file(char* path, libvig_access_t **accesses, unsigned *sz) {
+    FILE *fp;
+    
+    char *line = NULL;
+    char *line_ptr;
+    size_t len = 0;
+    ssize_t read_len;
+
+    libvig_access_t curr;
+
+    *sz = 0;
+    *accesses = NULL;
+    
+    if ((fp = fopen(path, "r")) == NULL) {
+        printf("[ERROR] Unable to open %s\n", path);
+        exit(1);
+    }
+
+    line = NULL;
+    while ((read_len = getline(&line, &len, fp)) > 0) {
+        if (len == 1) break;
+        line[read_len - 1] = 0;
+
+        if (match_token(line, "BEGIN")) {          
+            curr.dep_sz = 0;
+            curr.dep = NULL;
+        } else if (match_token(line, "END")) {
+            unique_save_access(curr, accesses, sz);
+        } else if (match_token(line, "layer")) {
+            line_ptr = line + strlen("layer");
+            while (*line_ptr == ' ') line_ptr++;
+
+            sscanf(line_ptr, "%u", &curr.layer);
+
+        } else if (match_token(line, "proto")) {
+            line_ptr = line + strlen("proto");
+            while (*line_ptr == ' ') line_ptr++;
+
+            sscanf(line_ptr, "%u", &curr.proto);
+
+        } else if (match_token(line, "dep")) {
+            line_ptr = line + strlen("dep");
+            while (*line_ptr == ' ') line_ptr++;
+
+            unsigned dep;
+
+            sscanf(line_ptr, "%u", &dep);
+            parse_dep(&curr, dep);
+
+        } else {
+            printf("[ERROR] Unknown token in line %s\n", line);
+            exit(1);
+        }
+    }
+
+    free(line);
+    fclose(fp);
+}
+
+int main(int argc, char* argv[]) {
+
+    if (argc < 2) {
+        printf("[ERROR] Missing arguments.");
+        printf("Please provide a libvig-access-out.txt file location\n");
+        return 1;
+    }
+
+    char* libvig_access_out = argv[1];
+
+    libvig_access_t *accesses;
+    unsigned sz;
+
+    parse_libvig_access_file(libvig_access_out, &accesses, &sz);
+
+    printf("Unique accesses (%u):\n", sz);
+    for (unsigned i = 0; i < sz; i++) {
+        for (unsigned idep = 0; idep < accesses[i].dep_sz; idep++) {
+            printf("* %s\n", R3S_pf_to_string(accesses[i].dep[idep]));
+        }
+    }
+}
