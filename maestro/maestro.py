9fb6c27e598f9bce0767ce8003dae57e71fa8966
Francisco Pereira <fchamicapereira@gmail.com>
2020-12-22 23:24:21 +0000

    meastro now fetches call paths and synthesizes automatically (only vignop, fow now)

--- maestro/maestro.py
+++ maestro/maestro.py
@@ -2,14 +2,39 @@
 
 import sys
 import pathlib
+import os
+import glob
+import re
+
+# env vars
+KLEE_DIR=os.getenv("KLEE_DIR")
+VIGOR_DIR=os.getenv("VIGOR_DIR")
+
+if not KLEE_DIR:
+	print("Missing KLEE_DIR env var. Exiting.")
+	exit(1)
+
+if not VIGOR_DIR:
+	print("Missing VIGOR_DIR env var. Exiting.")
+	exit(1)
 
 MAESTRO_DIR=pathlib.Path(__file__).parent.absolute()
-BOILERPLATE=f"{MAESTRO_DIR}/boilerplate.c"
-SYNTHESIZED=f"{MAESTRO_DIR}/build/maestro/nf.process.gen.c"
-PARALLEL_NF=f"{MAESTRO_DIR}/build/maestro/nf.c"
 
-INIT_TAG="@init"
-PROCESS_TAG="@process"
+BOILERPLATE = f"{MAESTRO_DIR}/boilerplate.c"
+SYNTHESIZED = f"{MAESTRO_DIR}/build/maestro/nf.process.gen.c"
+SYNTHESIZED_XML = f"{MAESTRO_DIR}/build/maestro/nf.process.gen.xml"
+PARALLEL_NF = f"{MAESTRO_DIR}/build/maestro/nf.c"
+
+NF="vignop"
+
+def synthesize_nf():
+	CALL_PATHS = glob.glob(f"{VIGOR_DIR}/vigor/{NF}/klee-last/*.call_path")
+	CALL_PATHS.sort(key=lambda f: int(re.sub('\D', '', f)))
+
+	LIBVIG_CALL_PATHS_TO_C_CODE = f"{KLEE_DIR}/build/bin/libvig-call-paths-to-C-code"
+	LIBVIG_CALL_PATHS_TO_C_CODE_ARGS = f"-out={SYNTHESIZED} -xml={SYNTHESIZED_XML} {' '.join(CALL_PATHS)}"
+
+	os.system(f"{LIBVIG_CALL_PATHS_TO_C_CODE} {LIBVIG_CALL_PATHS_TO_C_CODE_ARGS}")
 
 def stitch_synthesized_nf():
 	boilerplate_file = open(BOILERPLATE, mode='r')
@@ -26,4 +51,5 @@ def stitch_synthesized_nf():
 	parallel_file.write(parallel_content)
 	parallel_file.close()
 
+synthesize_nf()
 stitch_synthesized_nf()
