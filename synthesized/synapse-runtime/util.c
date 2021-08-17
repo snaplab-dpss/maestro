0b1f6946465397e14c1492306d424c7c17096745
João Maria Tiago <joao.marques.tiago@tecnico.ulisboa.pt>
2021-08-17 03:39:52 +0100

    update bvm2 boilerplate and runtime utils

--- synthesized/synapse-runtime/util.c
+++ synthesized/synapse-runtime/util.c
@@ -1,8 +1,140 @@
 #include "util.h"
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <inttypes.h>
+#include "synapse/runtime/wrapper/p4runtime/stream/handler/environment.hpp"
 
-// Stack manipulation
+#define LOOP_START_META(sz)                                                    \
+  printf("Metadata (%lu):\n", sz);                                             \
+  for (size_t i = 0; i < sz; i++) {
+
+#define LOOP_START_TAGS(sz)                                                    \
+  printf("Tags (%lu):\n", sz);                                                 \
+  for (size_t i = 0; i < sz; i++) {
+
+#define LOOP_END                                                               \
+  }                                                                            \
+  puts("")
+
+// Runtime configuration
+
+void print_meta(string_ptr_t name, string_ptr_t value) {
+  printf("<%.*s, \"", (int)name->sz, name->str);
+  for (size_t i = 0; i < value->sz; i++) {
+    printf("\\0%02x", value->str[i]);
+  }
+  printf("\">\n");
+}
+
+void print_tag(string_t name, uint32_t value) {
+  printf("<%.*s, %d>\n", (int)name.sz, name.str, value);
+}
+
+void print_meta_entry(pair_ptr_t entry) {
+  print_meta(entry->left, entry->right);
+}
+
+void print_tag_entry(pair_ptr_t entry) {
+  print_tag(*((string_ptr_t)entry->left),
+            ((p4_uint32_ptr_t)entry->right)->value);
+}
+
+void synapse_runtime_config_clear(synapse_config_t *config) {
+  config->devices = NULL;
+  config->devices_sz = 0;
+  config->tags_names = NULL;
+  config->tags_sz = 0;
+  config->tags = NULL;
+}
+
+void synapse_runtime_config_print(synapse_config_t *config) {
+  puts(":: Configuration summary ::\n");
+
+  printf("Devices (%lu):\t", config->devices_sz);
+  for (size_t i = 0; i < config->devices_sz; i++) {
+    printf("%" SCNu32 "%s", config->devices[i],
+           i < config->devices_sz - 1 ? ", " : "");
+  }
+  puts("");
+
+  if (config->tags_sz > 0) {
+    LOOP_START_TAGS(config->tags_sz)
+    print_tag(config->tags_names[i], config->tags[i]);
+    LOOP_END;
+  }
+}
+
+void synapse_runtime_pkt_in_clear(synapse_pkt_in_t *pkt_in) {
+  pkt_in->meta = NULL;
+  pkt_in->meta_sz = 0;
+  pkt_in->payload = NULL;
+}
+
+void synapse_runtime_pkt_in_print(synapse_pkt_in_t *pkt_in) {
+  puts(":: Packet-in summary ::\n");
+
+  if (NULL != pkt_in->payload) {
+    printf("Payload (%lu B):\t\"", pkt_in->payload->sz);
+    for (size_t i = 0; i < pkt_in->payload->sz; i++) {
+      printf("\\0%02x", pkt_in->payload->str[i]);
+    }
+    puts("\"");
+  }
+
+  if (pkt_in->meta_sz > 0) {
+    LOOP_START_META(pkt_in->meta_sz)
+    print_meta_entry(pkt_in->meta[i]);
+    LOOP_END;
+  }
+}
+
+void synapse_runtime_pkt_out_clear(synapse_pkt_out_t *pkt_out) {
+  pkt_out->payload = NULL;
+
+  pkt_out->meta = NULL;
+  pkt_out->meta_sz = 0;
+
+  pkt_out->tags = NULL;
+  pkt_out->tags_sz = 0;
+}
+
+void synapse_runtime_pkt_out_print(synapse_pkt_out_t *pkt_out) {
+  puts(":: Packet-out summary ::\n");
+
+  if (NULL != pkt_out->payload) {
+    printf("Payload (%lu B):\t\"", pkt_out->payload->sz);
+    for (size_t i = 0; i < pkt_out->payload->sz; i++) {
+      printf("\\0%02x", pkt_out->payload->str[i]);
+    }
+    puts("\"");
+  }
+
+  if (pkt_out->meta_sz > 0) {
+    LOOP_START_META(pkt_out->meta_sz)
+    print_meta_entry(pkt_out->meta[i]);
+    LOOP_END;
+  }
+
+  if (pkt_out->tags_sz > 0) {
+    LOOP_START_TAGS(pkt_out->tags_sz)
+    print_tag_entry(pkt_out->tags[i]);
+    LOOP_END;
+  }
+}
+
+// Environment manipulation
+
+bool synapse_get_helper_from_environment(env_ptr_t env, helper_ptr_t *helper) {
+  return NULL != helper &&
+         NULL != (*helper = synapse_runtime_environment_helper(env));
+}
+
+bool synapse_get_queue_from_environment(env_ptr_t env,
+                                        update_queue_ptr_t *queue) {
+  return NULL != env &&
+         NULL != (*queue = synapse_runtime_environment_queue(env));
+}
 
 bool synapse_get_stack_from_environment(env_ptr_t env, stack_ptr_t *stack,
                                         size_t expected_stack_size) {
@@ -11,28 +143,39 @@ bool synapse_get_stack_from_environment(env_ptr_t env, stack_ptr_t *stack,
          expected_stack_size == synapse_runtime_wrappers_stack_size(*stack);
 }
 
-bool synapse_extract_from_stack(stack_ptr_t stack, string_ptr_t *payload,
-                                pair_ptr_t **meta, size_t **meta_size) {
-  return NULL != stack &&
-         NULL != (*payload = synapse_runtime_wrappers_stack_pop(stack)) &&
-         NULL != (*meta_size = synapse_runtime_wrappers_stack_pop(stack)) &&
-         NULL != (*meta = synapse_runtime_wrappers_stack_pop(stack));
+// Stack manipulation
+
+bool synapse_populate_pkt_in_from_stack(stack_ptr_t stack,
+                                        synapse_pkt_in_t *pkt_in) {
+  if (NULL == stack) {
+    return false;
+  }
+
+  if (NULL == (pkt_in->payload = synapse_runtime_wrappers_stack_pop(stack))) {
+    return false;
+  }
+
+  pkt_in->meta_sz = *(size_t *)synapse_runtime_wrappers_stack_pop(stack);
+
+  if (NULL == (pkt_in->meta = synapse_runtime_wrappers_stack_pop(stack))) {
+    return false;
+  }
+
+  return true;
 }
 
-bool synapse_get_packet_in_metadata(pair_ptr_t *meta, size_t *meta_size,
-                                    string_t meta_name, string_ptr_t *result) {
-  if (NULL == meta || NULL == meta_size) {
+bool synapse_get_pkt_in_metadata(synapse_pkt_in_t *pkt_in, string_t meta_name,
+                                 string_ptr_t *result) {
+  if (NULL == pkt_in) {
     return false;
   }
 
   pair_ptr_t entry;
   string_ptr_t name;
 
-  for (size_t i = 0; i < *meta_size; i++) {
-    if (NULL == (entry = meta[i]) || NULL == (name = entry->left)) {
-      return false;
-    }
-
+  for (size_t i = 0; i < pkt_in->meta_sz && NULL != (entry = pkt_in->meta[i]) &&
+                     NULL != (name = entry->left);
+       i++) {
     if (0 == strcmp(name->str, meta_name.str)) {
       return NULL != (*result = entry->right);
     }
@@ -41,50 +184,74 @@ bool synapse_get_packet_in_metadata(pair_ptr_t *meta, size_t *meta_size,
   return false;
 }
 
-pair_ptr_t *alloc_pairs(stack_ptr_t stack, size_t pairs_sz) {
-  if (NULL == stack) {
-    return NULL;
+bool synapse_pkt_out_set_payload(synapse_pkt_out_t *pkt_out,
+                                 string_ptr_t payload) {
+  return NULL != pkt_out && NULL != (pkt_out->payload = payload);
+}
+
+bool synapse_pkt_out_set_meta(synapse_pkt_out_t *pkt_out, string_t name,
+                              string_t value) {
+  if (NULL == pkt_out ||
+      NULL == (pkt_out->meta = realloc(pkt_out->meta, pkt_out->meta_sz + 1))) {
+
+    SYNAPSE_ERROR("Could not allocate space for more metadata");
+    return false;
   }
 
-  pair_ptr_t *pairs = NULL;
-  if (NULL == (pairs = synapse_runtime_wrappers_stack_push(
-                   stack, malloc(pairs_sz * sizeof(pair_ptr_t))))) {
-    return NULL;
+  return NULL !=
+         (pkt_out->meta[pkt_out->meta_sz++] = synapse_runtime_wrappers_pair_new(
+              synapse_runtime_wrappers_string_new(name.str, name.sz),
+              synapse_runtime_wrappers_string_new(value.str, value.sz)));
+}
+
+bool synapse_pkt_out_set_tag(synapse_pkt_out_t *pkt_out, string_t name,
+                             uint32_t value) {
+  if (NULL == pkt_out ||
+      NULL == (pkt_out->tags = realloc(pkt_out->tags, pkt_out->tags_sz + 1))) {
+    SYNAPSE_ERROR("Could not allocate space for more tags");
+    return false;
   }
 
-  size_t *pairs_sz_dyn = NULL;
-  if (NULL == (pairs_sz_dyn = synapse_runtime_wrappers_stack_push(
-                   stack, malloc(sizeof(size_t))))) {
-    return NULL;
+  return NULL !=
+         (pkt_out->tags[pkt_out->tags_sz++] = synapse_runtime_wrappers_pair_new(
+              synapse_runtime_wrappers_string_new(name.str, name.sz),
+              synapse_runtime_wrappers_p4_uint32_new(value)));
+}
+
+bool synapse_pkt_out_flush(env_ptr_t env, synapse_pkt_out_t *pkt_out) {
+  stack_ptr_t stack;
+  if (!synapse_get_stack_from_environment(env, &stack, 0)) {
+    SYNAPSE_ERROR("The environment is corrupted");
+    return false;
   }
 
-  *pairs_sz_dyn = pairs_sz;
-  return pairs;
-}
+  /**
+   * tags size
+   * tags
+   * payload
+   * meta size
+   * meta
+   */
 
-pair_ptr_t *synapse_alloc_meta(stack_ptr_t stack, size_t meta_sz) {
-  return alloc_pairs(stack, meta_sz);
-}
+  size_t *meta_sz, *tags_sz;
+  if (NULL == (meta_sz = malloc(sizeof(size_t))) ||
+      NULL == (tags_sz = malloc(sizeof(size_t)))) {
+    SYNAPSE_ERROR("Failed to allocate space for meta and tags");
+    return false;
+  }
 
-pair_ptr_t *synapse_alloc_tags(stack_ptr_t stack, size_t tags_sz) {
-  return alloc_pairs(stack, tags_sz);
-}
+  synapse_runtime_wrappers_stack_push(stack, pkt_out->meta);
+  *meta_sz = pkt_out->meta_sz;
+  synapse_runtime_wrappers_stack_push(stack, meta_sz);
 
-pair_ptr_t *add_pair(pair_ptr_t *pairs, void *left, void *right) {
-  return (NULL == pairs ||
-          NULL == (*pairs++ = synapse_runtime_wrappers_pair_new(left, right)))
-             ? NULL
-             : pairs;
-}
+  synapse_runtime_wrappers_stack_push(stack, pkt_out->payload);
 
-pair_ptr_t *synapse_add_meta(pair_ptr_t *meta, string_t name, string_t value) {
-  return add_pair(meta, synapse_runtime_wrappers_string_new(name.str, name.sz),
-                  synapse_runtime_wrappers_string_new(value.str, value.sz));
-}
+  synapse_runtime_wrappers_stack_push(stack, pkt_out->tags);
+  *tags_sz = pkt_out->tags_sz;
+  synapse_runtime_wrappers_stack_push(stack, tags_sz);
 
-pair_ptr_t *synapse_add_tag(pair_ptr_t *tags, string_t name, uint32_t value) {
-  return add_pair(tags, synapse_runtime_wrappers_string_new(name.str, name.sz),
-                  synapse_runtime_wrappers_p4_uint32_new(value));
+  synapse_runtime_pkt_out_clear(pkt_out);
+  return true;
 }
 
 // Encoders
@@ -102,3 +269,57 @@ uint32_t synapse_decode_p4_uint32(string_ptr_t encoded) {
 uint16_t synapse_decode_port(string_ptr_t encoded) {
   return synapse_runtime_wrappers_decode_port(encoded)->port;
 }
+
+// Helpers
+
+bool synapse_queue_update(env_ptr_t env, p4_update_ptr_t update) {
+  update_queue_ptr_t queue = NULL;
+  if (!synapse_get_queue_from_environment(env, &queue)) {
+    return false;
+  }
+
+  return synapse_runtime_update_queue_queue(queue, update);
+}
+
+bool synapse_queue_configure_multicast_group(env_ptr_t env,
+                                             synapse_config_t *config) {
+  if (0 == config->devices_sz) {
+    return true;
+  }
+
+  helper_ptr_t helper = NULL;
+  if (!synapse_get_helper_from_environment(env, &helper)) {
+    return false;
+  }
+
+  p4_replica_ptr_t *replicas;
+  if ((0 != config->devices_sz && NULL == config->devices) ||
+      (NULL ==
+       (replicas = malloc(config->devices_sz * sizeof(p4_replica_ptr_t))))) {
+    return false;
+  }
+
+  size_t counter = 0;
+  for (size_t i = 0; i < config->devices_sz; i++) {
+    if (NULL == (replicas[i] = synapse_runtime_p4_replica_new(
+                     helper, config->devices[i], ++counter))) {
+      return false;
+    }
+  }
+
+  return synapse_queue_update(
+      env, synapse_runtime_p4_update_new(
+               helper, Update_Insert,
+               synapse_runtime_p4_entity_new(
+                   helper, Entity_PacketReplicationEngineEntry,
+                   synapse_runtime_p4_packet_replication_engine_entry_new(
+                       helper, synapse_runtime_p4_multicast_group_entry_new(
+                                   helper, SYNAPSE_MCAST_GROUP_ID, replicas,
+                                   config->devices_sz)))));
+}
+
+bool synapse_queue_insert_table_entry(env_ptr_t env) { return false; }
+
+bool synapse_queue_modify_table_entry(env_ptr_t env) { return false; }
+
+bool synapse_queue_delete_table_entry(env_ptr_t env) { return false; }
--- synthesized/synapse-runtime/util.h
+++ synthesized/synapse-runtime/util.h
@@ -3,37 +3,94 @@
 
 #include "synapse/runtime/p4runtime/stream/handler/environment.hpp"
 
-#define SYNAPSE_CPU_PORT (uint16_t)509
-#define SYNAPSE_DROP_PORT (uint16_t)510
-#define SYNAPSE_BROADCAST_PORT (uint16_t)511
-
 #define SYNAPSE_GRPC_ADDR "10.0.2.5:50051"
 #define SYNAPSE_ARGS_PATH "/home/user/vigor/synapse-runtime/controller"
 #define SYNAPSE_P4INFO_PATH SYNAPSE_ARGS_PATH "/program.p4info.txt"
 #define SYNAPSE_JSON_PATH SYNAPSE_ARGS_PATH "/program.json"
 
-// Global environment
+#define SYNAPSE_CPU_PORT (uint16_t)509
+#define SYNAPSE_DROP_PORT (uint16_t)510
+#define SYNAPSE_BROADCAST_PORT (uint16_t)511
 
-env_ptr_t g_env;
+#define SYNAPSE_MCAST_GROUP_ID 1
 
-// Stack manipulation
+// Runtime configuration
+
+typedef struct {
+  uint32_t *devices;
+  size_t devices_sz;
+
+  string_t *tags_names;
+  uint32_t *tags;
+  size_t tags_sz;
+
+} synapse_config_t;
+
+typedef struct {
+  string_ptr_t payload;
+
+  pair_ptr_t *meta;
+  size_t meta_sz;
+
+} synapse_pkt_in_t;
+
+typedef struct {
+  string_ptr_t payload;
+
+  pair_ptr_t *meta;
+  size_t meta_sz;
+
+  pair_ptr_t *tags;
+  size_t tags_sz;
+
+} synapse_pkt_out_t;
+
+bool synapse_runtime_config(synapse_config_t *config);
+
+void synapse_runtime_config_clear(synapse_config_t *config);
+
+void synapse_runtime_config_print(synapse_config_t *config);
+
+void synapse_runtime_pkt_in_clear(synapse_pkt_in_t *pkt_in);
+
+void synapse_runtime_pkt_in_print(synapse_pkt_in_t *pkt_in);
+
+void synapse_runtime_pkt_out_clear(synapse_pkt_out_t *pkt_out);
+
+void synapse_runtime_pkt_out_print(synapse_pkt_out_t *pkt_out);
+
+static synapse_config_t synapse_config;
+static synapse_pkt_in_t synapse_pkt_in;
+static synapse_pkt_out_t synapse_pkt_out;
+
+// Environment manipulation
+
+bool synapse_get_helper_from_environment(env_ptr_t env, helper_ptr_t *helper);
 
 bool synapse_get_stack_from_environment(env_ptr_t env, stack_ptr_t *stack,
                                         size_t expected_stack_size);
 
-bool synapse_extract_from_stack(stack_ptr_t stack, string_ptr_t *payload,
-                                pair_ptr_t **meta, size_t **meta_size);
+bool synapse_get_queue_from_environment(env_ptr_t env,
+                                        update_queue_ptr_t *queue);
 
-bool synapse_get_packet_in_metadata(pair_ptr_t *meta, size_t *meta_size,
-                                    string_t meta_name, string_ptr_t *result);
+// Stack manipulation
 
-pair_ptr_t *synapse_alloc_meta(stack_ptr_t stack, size_t meta_sz);
+bool synapse_populate_pkt_in_from_stack(stack_ptr_t stack,
+                                        synapse_pkt_in_t *pkt_in);
 
-pair_ptr_t *synapse_alloc_tags(stack_ptr_t stack, size_t tags_sz);
+bool synapse_get_pkt_in_metadata(synapse_pkt_in_t *pkt_in, string_t meta_name,
+                                 string_ptr_t *result);
 
-pair_ptr_t *synapse_add_meta(pair_ptr_t *meta, string_t name, string_t value);
+bool synapse_pkt_out_set_payload(synapse_pkt_out_t *pkt_out,
+                                 string_ptr_t payload);
 
-pair_ptr_t *synapse_add_tag(pair_ptr_t *tags, string_t name, uint32_t value);
+bool synapse_pkt_out_set_meta(synapse_pkt_out_t *pkt_out, string_t name,
+                              string_t value);
+
+bool synapse_pkt_out_set_tag(synapse_pkt_out_t *pkt_out, string_t name,
+                             uint32_t value);
+
+bool synapse_pkt_out_flush(env_ptr_t env, synapse_pkt_out_t *pkt_out);
 
 // Encoders
 
@@ -44,5 +101,16 @@ string_ptr_t synapse_encode_port(uint16_t value);
 uint32_t synapse_decode_p4_uint32(string_ptr_t encoded);
 
 uint16_t synapse_decode_port(string_ptr_t encoded);
+
+// Helpers
+
+bool synapse_queue_configure_multicast_group(env_ptr_t env,
+                                             synapse_config_t *config);
+
+bool synapse_queue_insert_table_entry(env_ptr_t env);
+
+bool synapse_queue_modify_table_entry(env_ptr_t env);
+
+bool synapse_queue_delete_table_entry(env_ptr_t env);
 
 #endif // VIGOR_SYNAPSE_RUNTIME_UTIL_H_
