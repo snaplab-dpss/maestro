c7cb9c1ab179141cffba21106174a7ea827d3eed
Francisco Pereira <fchamicapereira@gmail.com>
2021-09-14 15:53:41 +0100

    tm fix

--- libvig/unverified/expirator-tm.c
+++ libvig/unverified/expirator-tm.c
@@ -5,18 +5,10 @@
 
 int expire_items_tm(struct DoubleChainTM *chain, struct DoubleMap *map,
                     vigor_time_t time) {
-  bool *write_attempt_ptr = &RTE_PER_LCORE(write_attempt);
-  bool *write_state_ptr = &RTE_PER_LCORE(write_state);
-
   int count = 0;
   int index = -1;
 
   while (dchain_tm_expire_one_index(chain, &index, time)) {
-    if (!*write_state_ptr) {
-      *write_attempt_ptr = true;
-      return 1;
-    }
-
     dmap_erase(map, index);
     ++count;
   }
@@ -27,18 +19,10 @@ int expire_items_tm(struct DoubleChainTM *chain, struct DoubleMap *map,
 int expire_items_single_map_tm(struct DoubleChainTM *chain,
                                struct Vector *vector, struct Map *map,
                                vigor_time_t time) {
-  bool *write_attempt_ptr = &RTE_PER_LCORE(write_attempt);
-  bool *write_state_ptr = &RTE_PER_LCORE(write_state);
-
   int count = 0;
   int index = -1;
 
   while (dchain_tm_expire_one_index(chain, &index, time)) {
-    if (!*write_state_ptr) {
-      *write_attempt_ptr = true;
-      return 1;
-    }
-
     void *key;
     vector_borrow(vector, index, &key);
     map_erase(map, key, &key);
