357b68eeb2fbd72b825fadc6c77e8e75035a541d
Francisco Pereira <fchamicapereira@gmail.com>
2021-10-14 12:01:19 +0100

    revert change on explicit tcpudp endianess

--- libvig/verified/tcpudp_hdr.h
+++ libvig/verified/tcpudp_hdr.h
@@ -2,11 +2,10 @@
 #define _TCPUDP_H_INCLUDED_
 
 #include <stdint.h>
-#include <rte_byteorder.h>
 
 struct tcpudp_hdr {
-  rte_be16_t src_port;
-  rte_be16_t dst_port;
+  uint16_t src_port;
+  uint16_t dst_port;
 }
 #ifdef _NO_VERIFAST_
 __attribute__((__packed__)) // VeriFast does not understand attributes
