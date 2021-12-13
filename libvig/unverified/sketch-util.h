f624b9d0c1ba17d8b3e017e7068af3b14717f844
Francisco Pereira <fchamicapereira@gmail.com>
2021-12-13 14:01:05 +0000

    sketch using 8 hashes

--- libvig/unverified/sketch-util.h
+++ libvig/unverified/sketch-util.h
@@ -1,7 +1,7 @@
 #ifndef _SKETCH_UTIL_H_INCLUDED_
 
 // Careful: SKETCH_HASHES needs to be <= SKETCH_SALTS_BANK_SIZE
-#define SKETCH_HASHES 4
+#define SKETCH_HASHES 8
 #define SKETCH_SALTS_BANK_SIZE 8
 
 #include <stdint.h>
