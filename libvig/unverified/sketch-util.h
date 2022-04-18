56c16dbae19b787746f5b6d61f14a74922f43a15
Francisco Pereira <fchamicapereira@gmail.com>
2022-04-18 16:11:14 +0100

    increased number of available sketch hashes

--- libvig/unverified/sketch-util.h
+++ libvig/unverified/sketch-util.h
@@ -2,7 +2,7 @@
 
 // Careful: SKETCH_HASHES needs to be <= SKETCH_SALTS_BANK_SIZE
 #define SKETCH_HASHES 8
-#define SKETCH_SALTS_BANK_SIZE 32
+#define SKETCH_SALTS_BANK_SIZE 64
 
 #include <stdint.h>
 
