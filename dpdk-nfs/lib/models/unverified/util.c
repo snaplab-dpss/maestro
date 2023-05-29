7f49facb01de2cc1d2fb2673093ff884219ca4aa
Francisco Pereira <fchamicapereira@gmail.com>
2023-05-29 18:05:17 +0100

    tracing hash obj in

--- dpdk-nfs/lib/models/unverified/util.c
+++ dpdk-nfs/lib/models/unverified/util.c
@@ -6,6 +6,7 @@
 unsigned hash_obj(void *obj, int size_bytes) {
   klee_trace_ret();
   klee_trace_param_u64((uint64_t)obj, "obj");
+  klee_trace_param_tagged_ptr(obj, size_bytes, "obj", "obj", TD_IN);
   klee_trace_param_i32(size_bytes, "size");
   return klee_int("hash");
 }
